03143a56d4029b68cd8cf387bd68f6b0a00e05b3
유관우 <skymill2000@gmail.com>
2023-07-11 11:16:15 +0900

    Lecture(스크래핑): 테이블 데이터 스크래핑, 멜론 순위

--- scraping/test.py
+++ scraping/test.py
@@ -8,3 +8,9 @@ driver.get('https://www.etnews.com/20230710000227')
 
 title = driver.find_element(By.XPATH, '/html/body/section/section/article/div[1]/h2')
 print(title.text)
+
+titleWithClassName = driver.find_element(By.CLASS_NAME, 'article_title')
+print(titleWithClassName.text)
+
+contents = driver.find_element(By.XPATH, '//*[@id="articleBody"]/p')
+print(contents.text)
