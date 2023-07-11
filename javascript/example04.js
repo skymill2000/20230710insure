44d774260ac38594d778631089c000f7b5667014
유관우 <skymill2000@gmail.com>
2023-07-11 16:38:15 +0900

    Lecture(javascript): 배열의 선언과 요소에 접근

---
+++ javascript/example04.js
@@ -0,0 +1,8 @@
+let array = ["dfkejlfasfd", 1283128, { test: "test", age: 1000 }];
+console.log(array);
+console.log(array[0]);
+console.log(array[2].age);
+
+// java
+// private int[] numberArray = new int[5];
+// private List<String> stringList = new ArrayList();
