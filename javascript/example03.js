6cb9e349864b5c086396d983a5bbed3e69737fb9
유관우 <skymill2000@gmail.com>
2023-07-11 16:31:41 +0900

    Lecture(javascirpt): 자바 스크립트 메서드, 펑션 선언

---
+++ javascript/example03.js
@@ -0,0 +1,23 @@
+function plus(p1, p2) {
+  console.log(p1 + p2);
+}
+
+plus(3, 9);
+
+const minus = (p1, p2) => {
+  console.log(p1 - p2);
+};
+
+minus(3, 9);
+
+const mul = (p1, p2) => {
+  console.log(p1 * p2);
+};
+
+mul(3, 9);
+
+const div = (p1, p2) => {
+  console.log(p1 / p2);
+};
+
+div(3, 9);
