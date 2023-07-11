9718c5bb994be976302c83f850eb48405ff11c95
유관우 <skymill2000@gmail.com>
2023-07-11 14:51:49 +0900

    lecture(스크래핑): 개발 중간

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import re
+
+option = webdriver.ChromeOptions()
+option.add_experimental_option('detach', True)
+
 
 
 def rePlaceData(value):
@@ -12,15 +17,30 @@ def rePlaceData(value):
 
 
 def getAIAData(name, birth, gender):
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=option)
     scrapingResult = {
         'company': "AIA",
         'price': 0,
         'contents': ""
     }
     driver.implicitly_wait(3)
+
     driver.get(
         'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
     driver.implicitly_wait(3)
+
+    nameInput = driver.find_element(By.XPATH, '//*[@id="aia644363719"]')
+    nameInput.send_keys("19"+birth)
+
+    maleBtn = driver.find_element(By.XPATH, '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+    maleBtn.click()
+
+    resultBtn = driver.find_element(By.XPATH, '//*[@id="btn806817556"]')
+    resultBtn.click()
+
+    price = driver.find_element(By.XPATH, '//*[@id="premium-by-timespan-value"]');
+    print(price.text)
+
     return scrapingResult
 
+getAIAData("이름","890119",0)
