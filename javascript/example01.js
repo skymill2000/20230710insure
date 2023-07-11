123e09c52273102cd6c71c1b94e52e2a13563e24
유관우 <skymill2000@gmail.com>
2023-07-11 16:07:39 +0900

    Lecture(자바스크립트): 변수 선언

---
+++ javascript/example01.js
@@ -0,0 +1,17 @@
+var name = "홍길동";
+// inJava
+// private String name  = "ddd";
+
+var age = 100;
+// inJava
+// private int name  = 100;
+var age = 1000;
+
+let variableValue = "홍길동";
+variableValue = "둘리";
+// let variableValue = "테스트";
+
+const consValue = 1000;
+console.log(variableValue);
+console.log(consValue);
+console.log(age);
