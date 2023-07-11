cf15c7e0511db8aa9dbf5f6639afd52ea0f567c2
유관우 <skymill2000@gmail.com>
2023-07-11 15:16:35 +0900

    lecture(치아보험비교): 치아보험 비교 AIA 치아보험 결과값 전송 완료

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -25,22 +25,30 @@ def getAIAData(name, birth, gender):
     }
     driver.implicitly_wait(3)
 
-    driver.get(
-        'https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
-    driver.implicitly_wait(3)
+    driver.get('https://www.aia.co.kr/ko/our-products/medical-protection/non-par-denteal-health-plan.html#')
+    scrapingResult = {
+        'company': "AIA",
+        'price': 0,
+        'contents': []
+    }
+    
+    #AIA 생명 치과 보험 조회
+    textBox = driver.find_element(By.XPATH, '//*[@id="aia644363719"]')
+    textBox.send_keys('19'+birth)
+    
+    if gender == 1:
+        femaleBtn = driver.find_element(By.XPATH, '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[1]')
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element(By.XPATH, '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
+        maleBtn.click()
 
-    nameInput = driver.find_element(By.XPATH, '//*[@id="aia644363719"]')
-    nameInput.send_keys("19"+birth)
-
-    maleBtn = driver.find_element(By.XPATH, '//*[@id="calculator-container-form"]/div[1]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]')
-    maleBtn.click()
-
-    resultBtn = driver.find_element(By.XPATH, '//*[@id="btn806817556"]')
+    resultBtn = driver.find_element(By.XPATH,'//*[@id="btn806817556"]')
     resultBtn.click()
+    driver.implicitly_wait(2)
+    price = driver.find_element(By.XPATH, '//*[@id="premium-by-timespan-value"]')
+    resultValue = rePlaceData(price.text)
+    scrapingResult['price'] = resultValue
 
-    price = driver.find_element(By.XPATH, '//*[@id="premium-by-timespan-value"]');
-    print(price.text)
-
+    scrapingResult['contents'] = "보장내역..."
     return scrapingResult
-
-getAIAData("이름","890119",0)
