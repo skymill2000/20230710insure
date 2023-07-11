296c4ffee0d48091b8680a3e843e313734ab81db
유관우 <skymill2000@gmail.com>
2023-07-11 15:22:11 +0900

    lecture(치아보험): 라이나생명

--- scraping/dental_scraper.py
+++ scraping/dental_scraper.py
@@ -52,3 +52,46 @@ def getAIAData(name, birth, gender):
 
     scrapingResult['contents'] = "보장내역..."
     return scrapingResult
+
+def getRinaData(name, birth, gender):  
+    driver = webdriver.Chrome('./chromedriver')
+    scrapingResult = {
+        'company': "라이나",
+        'price': 0,
+        'contents': []
+    }
+    driver.get('https://direct.lina.co.kr/product/ess/dtc01/easy')
+    textBox = driver.find_element(By.XPATH,'//*[@id="birthday"]')
+    textBox.send_keys(birth)
+    if gender == 1:
+        femaleBtn = driver.find_element(By.XPATH,'//*[@id="main_btn_female"]')
+        femaleBtn.click()
+    else:
+        maleBtn = driver.find_element(By.XPATH,'//*[@id="main_btn_male"]')
+        maleBtn.click()
+    resultBtn = driver.find_element(By.XPATH,
+        '//*[@id="btn_direct_dental_cal"]')
+    resultBtn.click()
+    driver.implicitly_wait(3)
+
+    htmlResult = driver.find_element(By.XPATH,
+        '//*[@id="contents"]/div[2]/div[2]/div[2]/div/table/tbody[1]/tr[1]/td[2]/strong').text
+    resultValue = rePlaceData(htmlResult)
+
+    print(resultValue)
+
+    scrapingResult['price'] = resultValue
+    driver.implicitly_wait(2)
+    detailBtn = driver.find_element(By.XPATH,'//*[@id="openLayerplanPonA2"]')
+    detailBtn.click()
+    driver.implicitly_wait(2)
+
+    tableBody = driver.find_element(By.XPATH, '//*[@id="planPonA2"]/div/div[2]/div/div/table[1]').find_element(By.TAG_NAME,'tbody')
+    rows = tableBody.find_elements(By.TAG_NAME,"tr")
+    contentsList = []
+    for index, value in enumerate(rows):
+        if index != 0:
+            print(value.find_elements(By.TAG_NAME,'th')[0].text)
+            contentsList.append(value.find_elements(By.TAG_NAME,'th')[0].text)
+    scrapingResult['contents'] = contentsList
+    return scrapingResult
