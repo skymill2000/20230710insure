429432456b5b536a256848979c22d3b0b7764f1f
유관우 <skymill2000@gmail.com>
2023-07-12 10:56:07 +0900

    Lecture(React): 리액트 컴포넌트와 프롭스

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const Welcome = () => {
+  return `안녕하세요 홍길동님!`;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>테스트 콘텐츠</h1>
+        <Welcome></Welcome>
       </header>
     </div>
   );
