3dbd35b9e12df7645442d1528c9edebfec6ced53
유관우 <skymill2000@gmail.com>
2023-07-12 13:49:45 +0900

    Lecture(React): react-router-dom 설치 완료  1. npm install react-router-dom

--- insure/src/App.js
+++ insure/src/App.js
@@ -1,17 +1,13 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ListExample from "./components/ListExample";
-import QRcode from "./components/QRcode";
-import StateExample from "./components/StateExample";
-import StyledComponentExample from "./components/StyledComponentExample";
 
 function App() {
   return (
-    <div>
-      {/* <h1>테스트 콘텐츠</h1>
-      <StateExample></StateExample>
-      <ListExample></ListExample> */}
-      {/* <QRcode></QRcode> */}
-      <StyledComponentExample></StyledComponentExample>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ListExample />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
