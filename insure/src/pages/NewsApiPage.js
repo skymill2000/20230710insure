cbce419cb2f293d0cf1f8db7330d9df114fbe1c6
유관우 <skymill2000@gmail.com>
2023-07-12 15:27:05 +0900

    Lecture(React): #실습 1-1 search Input 컴포넌트 구성

--- insure/src/pages/NewsApiPage.js
+++ insure/src/pages/NewsApiPage.js
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import AppHeader from "../components/common/AppHeader";
+import SearchInput from "../components/news/SearchInput";
 
 const NewsApiPage = () => {
   const [searchText, setSearchText] = useState();
   const [searchResult, setSearchResult] = useState([]);
+  const handleChange = () => {
+    console.log("값이 변해요");
+  };
+  const handleClick = () => {
+    console.log("클릭이벤트 변해요");
+  };
 
   return (
     <div>
       <AppHeader title={"뉴스검색"}></AppHeader>
+      <SearchInput
+        handleChange={handleChange}
+        handleClick={handleClick}
+      ></SearchInput>
       {/* 
             <searchInput/>
             <newsList>        
