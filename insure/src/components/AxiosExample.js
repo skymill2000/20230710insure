a5c676a35ca696d8b2415758fb74212abad01cac
유관우 <skymill2000@gmail.com>
2023-07-12 14:40:00 +0900

    Lecture(React): axios 요청 샘플 작성

--- insure/src/components/AxiosExample.js
+++ insure/src/components/AxiosExample.js
@@ -1,7 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 const AxiosExample = () => {
-  const handleClick = () => {};
+  const handleClick = () => {
+    axios
+      .get("주소")
+      .then(function (response) {
+        // handle success
+        console.log(response);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  };
   return (
     <div>
       <button onClick={handleClick}>조회하기</button>
