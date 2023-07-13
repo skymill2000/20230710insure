b6b3398e8b1f4617d329a0f8d826c8009ec1e9b9
유관우 <skymill2000@gmail.com>
2023-07-13 11:43:47 +0900

    Lecture(건강보험심사평가원API): 병원 진료 상위과목 표기 페이지 추가

--- insure/src/App.js
+++ insure/src/App.js
@@ -4,6 +4,7 @@ import QRcode from "./components/QRcode";
 import AxiosExample from "./components/AxiosExample";
 import NewsApiPage from "./pages/NewsApiPage";
 import HospitalSearchPage from "./pages/HospitalSearchPage";
+import HospitalDetail from "./pages/HospitalDetailPage";
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
         <Route path="/axios" element={<AxiosExample />}></Route>
         <Route path="/news" element={<NewsApiPage />}></Route>
         <Route path="/hospital" element={<HospitalSearchPage />}></Route>
+        <Route
+          path="/hospitalDetail"
+          element={<HospitalDetail></HospitalDetail>}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ insure/src/pages/HospitalDetailPage.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+
+const HospitalDetail = () => {
+  const { search } = useLocation();
+  console.log("location정보:", useLocation());
+  const [result, setResult] = useState();
+  const { ykiho } = queryString.parse(search);
+
+  useEffect(() => {
+    getHospitalDetailData();
+  }, []);
+
+  const getHospitalDetailData = () => {
+    console.log("콤포넌트가 마운트 될때 실행됩니다.");
+    let option = {
+      //요청 작성
+    };
+    axios(option).then(({ data }) => {
+      console.log(data.response.body.items.item);
+      setResult(data.response.body.items.item);
+    });
+  };
+
+  return (
+    <div>
+      {result !== undefined && (
+        <>
+          <p>{result.mfrnIntrsIlnsCdNm1}</p>
+          <p>{result.mfrnIntrsIlnsCdNm2}</p>
+          <p>{result.mfrnIntrsIlnsCdNm3}</p>
+          <p>{result.mfrnIntrsIlnsCdNm4}</p>
+          <p>{result.mfrnIntrsIlnsCdNm5}</p>
+        </>
+      )}
+      {result === undefined && (
+        <>
+          <p>현재 등록된 데이터가 없습니다</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default HospitalDetail;
